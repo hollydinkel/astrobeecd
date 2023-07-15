a57ce47cf6ee62ff9b5e3b215c42c04b722554ba
JeffersonAero <holly.dinkel@gmail.com>
2023-07-15 17:10:50 -0500

    update directory structure to pass in robot name

--- process_sequential_poses.py
+++ process_sequential_poses.py
@@ -8,11 +8,12 @@ import argparse
 parser = argparse.ArgumentParser(description="Process sequential poses.")
 parser.add_argument("survey", help="Indicate survey number")
 parser.add_argument("date", help="Dataset date.")
+parser.add_argument("robot", help="Robot name")
 
 args = parser.parse_args()
 
-img_dir = f"./data/{args.date}/bayer/{args.survey}"
-poses_dir = f"./data/{args.date}/pose/{args.survey}"
+img_dir = f"./data/{args.date}/{args.robot}/bayer/survey{args.survey}"
+poses_dir = f"./data/{args.date}/{args.robot}/pose/survey{args.survey}"
 
 pose_list = [int(os.path.splitext(file)[0]) for file in os.listdir(poses_dir)]
 img_list = [int(os.path.splitext(file)[0]) for file in os.listdir(img_dir)]
