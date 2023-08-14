edbbd819c03911b21a27686555cb1c454605e824
hollydinkel <holly.dinkel@gmail.com>
2023-08-14 17:25:02 -0500

    [FIX] fix data directory structure, store zipped data on gdrive, download data in correct directory structure from gdrive in run.sh

--- run.sh
+++ run.sh
@@ -4,6 +4,11 @@ export SURVEY=1
 export DATE=20230419
 export ROBOT=bsharp
 
+# Download data
+cd $DATA && gdown https://drive.google.com/uc?id=1qNCRl9XceINrHLFUp4pBti9I4oJO2zf1
+unzip data.zip
+rm data.zip
+
 cd $DATA && python3 scripts/poses_to_file.py $SURVEY $DATE $ROBOT
 cd $ASTROBEE_WS && source ../devel/setup.bash
 rosrun sparse_mapping process_sequential_images.py $DATA/data/$DATE/$ROBOT/bayer/survey$SURVEY $ASTROBEE_WS/src/astrobee/config
