baa917fef77f2bafbb3b5e129732b83df307e012
hollydinkel <holly.dinkel@gmail.com>
2023-08-20 21:00:02 -0500

    [FIX] fix sourcing setup.bash in run.sh

--- run.sh
+++ run.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 export ASTROBEE_WS=/src/astrobee/src
+export DEVEL=/src/astrobee/devel
 export DATA=$ASTROBEE_WS/astrobee_data_processing
 export DATE=20230419
 export ROBOT=bsharp
+source $DEVEL/setup.bash
 
 # Install dependencies
-pip install pandas
 apt-get update
 apt-get install -y python3-pip
-pip3 install gdown pyquaternion
+pip install numpy --upgrade
+pip install gdown pyquaternion pandas
 
-# # Download data
+# Download data
 cd $DATA && gdown https://drive.google.com/uc?id=1qNCRl9XceINrHLFUp4pBti9I4oJO2zf1
 unzip data.zip
 rm data.zip
@@ -18,9 +20,7 @@ rm data.zip
 for SURVEY_NUMBER in 1 2 3 4
 do
     export SURVEY=$SURVEY_NUMBER
-    echo $SURVEY
     cd $DATA && python3 scripts/poses_to_file.py $SURVEY $DATE $ROBOT
-    cd $ASTROBEE_WS && source ../devel/setup.bash
     rosrun sparse_mapping process_sequential_images.py $DATA/data/$DATE/$ROBOT/bayer/survey$SURVEY $ASTROBEE_WS/src/astrobee/config
     cd $DATA && python3 scripts/process_sequential_poses.py $SURVEY $DATE $ROBOT
     cd $DATA/data/$DATE/$ROBOT/bayer/survey$SURVEY
