8470e5fdd509445c6842fcb02f44e4a140a0e817
hollydinkel <holly.dinkel@gmail.com>
2023-08-20 19:53:25 -0500

    [FIX] fix run.sh for loop

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 export ASTROBEE_WS=/src/astrobee/src
 export DATA=$ASTROBEE_WS/astrobee_data_processing
 export DATE=20230419
@@ -8,7 +9,7 @@ apt-get update
 apt-get install -y python3-pip
 pip install gdown pyquaternion
 
-# Download data
+# # Download data
 cd $DATA && gdown https://drive.google.com/uc?id=1qNCRl9XceINrHLFUp4pBti9I4oJO2zf1
 unzip data.zip
 rm data.zip
@@ -16,9 +17,11 @@ rm data.zip
 for SURVEY_NUMBER in 1 2 3 4
 do
     export SURVEY=$SURVEY_NUMBER
+    echo $SURVEY
     cd $DATA && python3 scripts/poses_to_file.py $SURVEY $DATE $ROBOT
     cd $ASTROBEE_WS && source ../devel/setup.bash
     rosrun sparse_mapping process_sequential_images.py $DATA/data/$DATE/$ROBOT/bayer/survey$SURVEY $ASTROBEE_WS/src/astrobee/config
     cd $DATA && python3 scripts/process_sequential_poses.py $SURVEY $DATE $ROBOT
     cd $DATA/data/$DATE/$ROBOT/bayer/survey$SURVEY
     ls -v | nl -v 0 | while read n f; do mv -n "$f" "Image$n.JPG"; done
+done
